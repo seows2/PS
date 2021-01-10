8ccf4e24453c7f801790266a6e16cb5e953fd71d
SeoWS <seows2@naver.com>
2021-01-11 01:59:41 +0900

    정수 삼각형

---
+++ level3/정수 삼각형.js
@@ -0,0 +1,10 @@
+function solution(triangle) {
+  for (let i = triangle.length - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      triangle[i][j] += Math.max(triangle[i + 1][j + 1], triangle[i + 1][j]);
+    }
+  }
+  return triangle[0][0];
+}
+
+solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]);
