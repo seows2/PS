30ad3cfea2481542cc7fb7b0b4b6852fbb1b6591
SeoWS <seows2@naver.com>
2021-01-15 23:58:32 +0900

    2675_문자열 반복

---
+++ BOJ/2675_문자열 반복.js
@@ -0,0 +1,24 @@
+/*
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+*/
+const input = ["2", "3 ABC", "5 /HTP"];
+
+input.shift();
+
+function solution(input) {
+  let answer = [];
+  let temp = "";
+  for (const words of input) {
+    const [repeat, word] = words.split(" ");
+    [...word].forEach((e) => {
+      const repeatWord = e.repeat(repeat);
+      temp += repeatWord;
+    });
+    answer.push(temp);
+    temp = "";
+  }
+  console.log(answer.join("\n"));
+}
+
+solution(input);
--- BOJ/ex.js
+++ BOJ/ex.js
@@ -1,10 +1,10 @@
 /*
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 */
- const input = ["10 5", "1 10 4 9 2 3 8 5 7 6"]
+const input = ["10 5", "1 10 4 9 2 3 8 5 7 6"];
 
-const [N, X] = input[0].split(" ").map(_ => Number(_))
-const A = input[1].split(" ").map(_ => Number(_))
+const [N, X] = input[0].split(" ").map((_) => Number(_));
+const A = input[1].split(" ").map((_) => Number(_));
 
-console.log(A.filter(e => e<X).join(" "));
+console.log(A.filter((e) => e < X).join(" "));
