cd7de5b2feacd4fdeecd1c579fb86227f78f0b47
SeoWS <seows2@naver.com>
2021-01-22 16:33:45 +0900

    가장 먼 노드 BFS

--- level3/가장 먼 노드.js
+++ level3/가장 먼 노드.js
@@ -1,31 +1,40 @@
 function solution(n, edge) {
-  const graph = Array.from({length: n}, () => [])
+  const graph = Array.from({ length: n }, () => []);
 
-  edge.forEach(([a,b]) => {
-    graph[a-1].push(b-1)
-    graph[b-1].push(a-1)
+  edge.forEach(([a, b]) => {
+    graph[a - 1].push(b - 1);
+    graph[b - 1].push(a - 1);
   });
-  //console.log(graph);
+  console.log(graph);
 
-  const queue = [[0,0]]
-  const visited = [true]
-  const depNum = []
+  const queue = [[0, 0]];
+  const visited = [true];
+  const depNum = [];
 
   while (queue.length) {
-    const [cur, dep] = queue.shift()
-    //console.log(cur, dep, queue)
-    depNum[dep] = depNum[dep] ? depNum[dep]+1 : 1
-    graph[cur].forEach(e => {
-      if(!visited[e]){
-        visited[e] = true
-        queue.push([e, dep+1])
+    const [cur, dep] = queue.shift();
+    console.log(cur, dep, queue);
+    depNum[dep] = depNum[dep] ? depNum[dep] + 1 : 1;
+    graph[cur].forEach((e) => {
+      if (!visited[e]) {
+        visited[e] = true;
+        queue.push([e, dep + 1]);
       }
-    })
+    });
   }
-  return depNum[depNum.length-1]
+  console.log(depNum);
+  return depNum[depNum.length - 1];
 }
 
-solution(6,	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]])
+solution(6, [
+  [3, 6],
+  [4, 3],
+  [3, 2],
+  [1, 3],
+  [1, 2],
+  [2, 4],
+  [5, 2],
+]);
 
 /* [
     [ 3, 6 ], [ 4, 3 ],
