98b82c478f2d9a9e0cf3e3f585960a6d85529da0
SeoWS <seows2@naver.com>
2021-02-10 18:04:35 +0900

    입국심사

--- level3/입국심사.js
+++ level3/입국심사.js
@@ -1,4 +1,14 @@
 function solution(n, times) {
-  var answer = 0;
-  return answer;
+  times.sort((a, b) => a - b);
+  let left = 1;
+  let right = n * times[times.length - 1];
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let cnt = times.reduce((a, b) => a + Math.floor(mid / b), 0);
+    if (cnt >= n) right = mid - 1;
+    else left = mid + 1;
+  }
+  return left;
 }
+solution(6, [7, 10]);
