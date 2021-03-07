7035de9650d891ba5fa9532b61206260d0a997d7
seows2 <seows2@naver.com>
2021-03-07 15:40:58 +0900

    시험 감독.js

---
+++ BOJ/삼성SW역량/시험 감독.js
@@ -0,0 +1,13 @@
+/*
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+*/
+
+const input = ["3","3 4 5","2 2"];
+
+function solution(input) {
+    const n = input.shift()
+    const 
+}
+
+solution(input);
