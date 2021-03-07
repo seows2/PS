cc96af31a3e86ad794a22e7d24346504f87ffec4
seows2 <seows2@naver.com>
2021-03-08 01:39:12 +0900

    시험 감독

--- BOJ/삼성SW역량/시험 감독.js
+++ BOJ/삼성SW역량/시험 감독.js
@@ -7,7 +7,16 @@ const input = ["3","3 4 5","2 2"];
 
 function solution(input) {
     const n = input.shift()
-    const 
+    const [main, sub] = input[1].split(" ").map(e => Number(e))
+    const exam = input[0].split(" ").map(e => Number(e)-main)
+    let answer = Number(n)
+
+    exam.forEach(e => {
+        if(e > 0){
+            answer += Math.ceil(e / sub)
+        }
+    });
+    console.log(answer);
 }
 
 solution(input);
