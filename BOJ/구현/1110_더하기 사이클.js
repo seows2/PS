e146f0c99f0f7f2dca986f5e51a02327baeb70e9
seows2 <seows2@naver.com>
2021-03-24 19:26:26 +0900

    1110_더하기 사이클

--- BOJ/구현/1110_더하기 사이클.js
+++ BOJ/구현/1110_더하기 사이클.js
@@ -3,8 +3,29 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 */
 
-const input = "baekjoon";
+const input = "0";
 
-function solution(input) {}
+function solution(input) {
+    const originNum = input
+    let cnt = 0
+    let newNum = originNum.length === 2 ? originNum : 0+originNum
+
+    while (true) {
+        cnt++
+        const numSum = String([...newNum].map(e => Number(e)).reduce((a,b) => a+b, 0));
+        if(numSum.length === 2){
+            newNum = String(Number(newNum[1] + numSum[1]))
+        } else {
+            newNum = String(Number(newNum[1] + numSum[0]))
+        }
+        if(newNum === originNum){
+            break;
+        }
+        newNum = newNum.length === 2 ? newNum : 0+newNum
+    }
+    
+
+    console.log(cnt);
+}
 
 solution(input);
