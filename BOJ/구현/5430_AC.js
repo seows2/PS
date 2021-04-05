adfa6289a82ab26bedfd7284d5b0356dfd42f02f
seows2 <seows2@naver.com>
2021-04-05 18:41:14 +0900

    5430_AC 코드 변경

--- BOJ/구현/5430_AC.js
+++ BOJ/구현/5430_AC.js
@@ -3,18 +3,29 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 */
 
-const input = ["3","DDRDRD","4","[100,100,99,40]","D","0","[]","RDD","4","[1,2,3,4]"];
+const input = ["4", "RDD","4","[1,2,3,4]","DD","1","[42]","RRD","6","[1,1,2,3,5,8]","D","0","[]"];
 
 function solution(input) {
     const T = Number(input.shift());
     const answer = [];
     for (let i = 0; i < T; i++) {
         const [P, N, arr] = input.splice(0, 3);
-        let nums = [...arr].splice(1, arr.length-2).join("").split(",");
+        let nums = arr.slice(1, arr.length-1).split(",");
         if(N === "0") {nums = [];}
+
+        /*
+         * RR이 연속으로 나오는 명령어는 불필요한 연산이므로 전부 없애버린다.
+         * DRRD => DD
+         */
         const regex = new RegExp(/R{2}/,"g");
         const commands = P.replace(regex,"")
 
+
+        /*
+         * dir 뒤집기 연산 판단. True면 정방향 False면 역방향
+         * left 왼쪽에서 지워야할 요소
+         * right 오른쪽에서 지워야할 요소
+         */
         let dir = true;
         let left = 0;
         let right = 0;
@@ -22,12 +33,15 @@ function solution(input) {
             if(command === "R") {
                 dir = !dir;
             } else {
+                //command가 "D"
                 if(dir) left += 1;
                 else right += 1;
             }
         }
-        console.log(left, N - right);
-
+        
+        /*
+         * 지워야할 총 갯수(left+right) 배열의 길이(N) 보다 크면 error
+         */
         if(left + right <= N){
             nums = nums.slice(left, N - right);
             if(dir) answer.push("[" + nums.join(",") + "]");
