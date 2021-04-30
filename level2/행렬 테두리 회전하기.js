34dfc121376bbec2a6220b58facdadd3d31fc6a2
seows2 <seows2@naver.com>
2021-05-01 00:16:50 +0900

    행렬 테두리 회전

--- level2/행렬 테두리 회전하기.js
+++ level2/행렬 테두리 회전하기.js
@@ -1,20 +1,55 @@
 function solution(rows, columns, queries) {
     const rotate = (query, map) => {
         const [x1, y1, x2, y2] = query;
-        const origin = map[x1 - 1][y1 - 1];
-        let temp = [];
-        console.log(map, temp);
+        const top = [];
+        const right = [];
+        const left = [];
+        const bottom = [];
 
+        //Top
+        for (let i = y1 - 1; i < y2 - 1; i++) {
+            top.push(map[x1 - 1][i])
+        }
+        //Right
+        for (let i = x1 - 1; i < x2 - 1; i++) {
+            right.push(map[i][y2 - 1])
+        }
+        //Bottom
+        for (let i = y1; i < y2; i++) {
+            bottom.push(map[x2 - 1][i])
+        }
+        //Left
+        for (let i = x1; i < x2; i++) {
+            left.push(map[i][y1 - 1])
+        }
 
+        //Top
+        map[x1 - 1].splice(y1, y2 - y1, ...top);
+        //Right
+        let j = 0;
+        for (let i = x1; i < x2; i++) {
+            map[i].splice(y2 - 1, 1, right[j++])
+        }
+        //Bottom
+        map[x2 - 1].splice(y1 - 1, y2 - y1, ...bottom);
+        //Left
+        j = 0;
+        for (let i = x1 - 1; i < x2 - 1; i++) {
+            map[i].splice(y1 - 1, 1, left[j++])
+        }
+        console.log(map);
+        return Math.min(...top, ...right, ...left, ...bottom)
 
     }
-    const map = Array.from({ length: rows }, (_, row) => Array.from({ length: columns }, (_, i) => rows * row + i + 1));
+    const map = Array.from({ length: rows }, (_, row) => Array.from({ length: columns }, (_, i) => columns * row + i + 1));
     let rotatedMap = map.slice();
-    console.log(map);
+    const answer = []
+    //console.log(map);
     queries.forEach((query) => {
         const min = rotate(query, rotatedMap);
-        console.log(min);
+        answer.push(min)
     })
+    return answer
 }
 
 solution(6, 6, [[2, 2, 5, 4], [3, 3, 6, 6], [5, 1, 6, 3]])
