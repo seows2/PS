2eed329424fb8a3f95aa82ae7e5db7d5a189aa22
seows2 <seows2@naver.com>
2021-05-18 14:55:54 +0900

    나무 자르기(un)

---
+++ BOJ/이분 탐색/2805_나무 자르기(unsol).js
@@ -0,0 +1,36 @@
+/*
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+*/
+
+const input = ["5 7", "20 15 10 17"];
+
+function solution(input) {
+    const check = (mid, M, treeHeights) => {
+        let sum = 0;
+        for (const height of treeHeights) {
+            if (height < mid) continue;
+            const remain = height - mid;
+            sum += remain
+        }
+
+        if (sum >= M) return true;
+
+        return false;
+    }
+    const [N, M] = input[0].split(" ").map(e => Number(e));
+    const treeHeights = input[1].split(" ").map(e => Number(e));
+    let left = 1;
+    let right = Number.MAX_SAFE_INTEGER
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        if (check(mid, M, treeHeights)) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    console.log(right);
+}
+
+solution(input);
