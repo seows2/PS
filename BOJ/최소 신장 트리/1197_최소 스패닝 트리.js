f73f9b7419bc73359bbf1634de07521c99759732
seows2 <seows2@naver.com>
2021-05-23 14:11:22 +0900

    1197_최소 스패닝 트리

--- BOJ/최소 신장 트리/1197_최소 스패닝 트리.js
+++ BOJ/최소 신장 트리/1197_최소 스패닝 트리.js
@@ -5,19 +5,34 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const input = ["3 3", "1 2 1", "2 3 2", "1 3 3"];
 
+//크루스칼
 function solution(input) {
-    const makeGraph = (V, arr) => {
-        const graph = Array.from({ length: V }, () => []);
-        arr.forEach(([from, to, value]) => {
-            graph[from - 1].push([to - 1, value]);
-            graph[to - 1].push([from - 1, value]);
-        })
-        return graph;
+    const find = (u) => {
+        if (u === parent[u]) return u;
+
+        return parent[u] = find(parent[u]);
+    }
+    const merge = (u, v) => {
+        isCycle = false;
+        u = find(u);
+        v = find(v);
+
+        if (u === v) return;
+
+        parent[u] = v;
+        isCycle = true;
     }
     const [V, E] = input.shift().split(" ").map(e => Number(e));
-    const arr = input.map(e => e.split(" ").map(e => Number(e)));
-    const graph = makeGraph(V, arr);
-    console.log(graph);
+    const parent = Array.from({ length: V + 1 }, (_, i) => i);
+    const arr = input.map(e => e.split(" ").map(e => Number(e))).sort((a, b) => a[2] - b[2]);
+    let isCycle = false;
+    let answer = 0;
+
+    arr.forEach(([from, to, value]) => {
+        merge(from, to);
+        if (isCycle) answer += value;
+    })
+    console.log(answer);
 
 }
 
