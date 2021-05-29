126bebf2f00fa13d60887c9b41383be4fc5523e8
seows2 <seows2@naver.com>
2021-05-30 03:44:24 +0900

    unsol

---
+++ BOJ/분할 정복/1629_곱셈(unsol).js
@@ -0,0 +1,14 @@
+/*
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+*/
+
+const input = "10 11 12";
+
+function solution(input) {
+    const [A,B,C] = input.split(" ").map(e => Number(e));
+
+
+}
+
+solution(input);
